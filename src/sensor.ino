7b9040a686599869fb252acccdd4c504ad788b15
moonshot <moonshot2x@gmail.com>
2015-03-18 08:32:47 -0400

    Simplify the onewire calls.

--- src/sensor.ino
+++ src/sensor.ino
@@ -1,116 +1,68 @@
-// This #include statement was automatically added by the Spark IDE.
 #include "OneWire.h"
 
-float celsius;
-char cel[8] = "NONE";
-char far[8] = "NONE";
-int temp_sensor = D3;
+uint8_t rom[8];
+uint8_t resp[9];
+char result[16] = "NONE";
+char device[16] = "NONE";
 
-OneWire ds = OneWire(temp_sensor);  // on pin D3 (a 4.7K resistor is necessary)
+int temp_sensor = D3;
+OneWire d3 = OneWire(temp_sensor);  // DS18B20 on pin D3
+
+#define publish_delay 60000
+unsigned int lastPublish = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Spark.variable("cel",&cel,STRING);
-  Spark.variable("far",&far,STRING);
+  Spark.variable("result", &result, STRING);
+  Spark.variable("device", &device, STRING);
   pinMode(temp_sensor, INPUT);
 }
 
 void loop() {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
 
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(5000);
-    ds.reset();
-    ds.skip();
-    ds.write(0x44);
-    Serial.println("Converting temp on all probes");
-    Serial.println();
-    delay(1000);
+  unsigned long now = millis();
+
+  if ((now - lastPublish) < publish_delay) {
     return;
   }
 
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
+  d3.reset_search(); // Start back at the beginning
+  if ( !d3.reset() ) {
+    sprintf(device, "NO DEVICE");
+    delay(5000);
+    return;
+   }
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
+  d3.skip(); // no need to search, only 1 device on the bus
+  d3.write(0x44,1);  // start conversation, with parasite power on at the end
+  delay(1000);  // maybe 750ms is enough, maybe not
 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  }
+  // Get the ROM address
+  d3.reset();
+  d3.write(0x33);
+  d3.read_bytes(rom, 8);
+  // Get the temp
+  d3.reset();
+  d3.write(0x55);
+  d3.write_bytes(rom,8);
+  d3.write(0x44);
+  delay(10);
+  d3.reset();
+  d3.write(0x55);
+  d3.write_bytes(rom, 8);
+  d3.write(0xBE);
+  d3.read_bytes(resp, 9);
 
-  // start conversion, with parasite power on at the end
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
+  byte MSB = resp[1];
+  byte LSB = resp[0];
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
+  int16_t intTemp = ((MSB << 8) | LSB); //using two's compliment 16-bit
+  float celsius =   ((double)intTemp)/16.0;
+  float fahrenheit = (( celsius*9.0)/5.0+32.0);
 
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  sprintf(cel,"%f.1", celsius);
-  fahrenheit = celsius * 1.8 + 32.0;
-  sprintf(far,"%f.1", fahrenheit);
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
+  sprintf(result, "%2.2f", fahrenheit);
+  sprintf(device, "%x%x%x%x%x%x%x%x", rom[0], rom[1],
+    rom[2], rom[3], rom[4], rom[5], rom[6], rom[7]);
+
+  Spark.publish(device, String(result), 60, PRIVATE);
+  lastPublish = now;
 }
